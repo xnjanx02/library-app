f06a65ea7930a21a537ef48634b28771ac0c943f
xnjanx02 <anjana.jk009@gmail.com>
2022-07-07 17:09:40 +0530

    Project created!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddBooksComponent } from './add-books/add-books.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddBooksComponent
   ],
   imports: [
     BrowserModule,
