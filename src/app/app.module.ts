ddfc6acf7fc58021aee2accb561c295bcea48d84
xnjanx02 <anjana.jk009@gmail.com>
2022-07-07 17:33:16 +0530

    Search book component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddBooksComponent } from './add-books/add-books.component';
+import { SearchBooksComponent } from './search-books/search-books.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddBooksComponent
+    AddBooksComponent,
+    SearchBooksComponent
   ],
   imports: [
     BrowserModule,
